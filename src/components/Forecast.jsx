1d22d5c7ba702aacde021f63fd4c3dfe720c6db4
MykytaOksak <stupidmaadworld@gmail.com>
2021-09-19 03:48:14 +0300

    forecast fix

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -4,8 +4,6 @@ export default function Forecast(props) {
     // hooks
     const [apiData, setApiData] = useState({});
     const [city, setCity] = useState();
-    const [lat, setLat] = useState([]);
-    const [long, setLong] = useState([]);
 
     // api key and url
     const apiKey = "51b91d99f95843f1d1cc5913483efaa0";
@@ -26,7 +24,6 @@ export default function Forecast(props) {
     return (
         <div id="body-container" class="md:max-w-xl md:mx-auto grid grid-flow-rows gap-20 px-15 pb-80 pt-150 md:gap-20 md:px-20 md:pb-70 md:pt-120 text-center">
             <input
-                id="input-email"
                 class="transition px-20 py-10 duration-500 ease-in-out focus:outline-none focus:ring-4 md:focus:ring-2 focus:ring-white 
                 focus:ring-opacity-30 md:text-p2-md text-primary font-medium rounded-full bg-black bg-opacity-30"
                 type="text"
